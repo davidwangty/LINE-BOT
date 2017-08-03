a64ca5827503c13c2c2a37c51dff876dd97696b6
davidwangty <davidwangty@gmail.com>
2017-08-03 17:09:06 +0800

    yen search

--- index.py
+++ index.py
@@ -10,16 +10,31 @@ from linebot.models import (
     MessageEvent, TextMessage, TextSendMessage,
 )
 import os
+import psycopg2
+import urlparse
+import currency_search
+from datetime import datetime
+
+# Database
+urlparse.uses_netloc.append("postgres")
+url = urlparse.urlparse(os.environ["DATABASE_URL"])
+
+conn = psycopg2.connect(
+    database=url.path[1:],
+    user=url.username,
+    password=url.password,
+    host=url.hostname,
+    port=url.port
+)
 
 AccessToken = os.environ["ChannelAccessToken"]
 ChannelSecret = os.environ["ChannelSecret"]
+ChannelID = os.environ["ChannelID"]
 
 app = Flask(__name__)
 
 line_bot_api = LineBotApi(AccessToken)
 handler = WebhookHandler(ChannelSecret)
-
-
 
 # Echo功能
 @app.route("/callback", methods=['POST'])
@@ -41,10 +56,24 @@ def callback():
 
 
 @handler.add(MessageEvent, message=TextMessage)
-def handle_message(event):
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=event.message.text))
+def handel_message(event):
+    if event.message.text == "日幣":
+        yen_info = currency_search.yen()
+        message = "日幣 " + datetime.strftime(datetime.now(), "%m/%d %H:%M")
+        message += "\n 現金買入  賣出  即期買入  賣出\n"
+        for info in yen_info:
+            message += " " + info + "  "
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text=message))
+    else:
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text=event.message.text))
+# def handle_message(event):
+#     line_bot_api.reply_message(
+#         event.reply_token,
+#         TextSendMessage(text=event.message.text))
 
 
 if __name__ == "__main__":
